f93f7af85ea7524bd245e7b8b53d7722ffa760ef
Brad Daily <brad@bradleyboy.com>
2024-12-01 17:05:00 -0500

    refactoring day1 with claude's feedback

--- src/main.zig
+++ src/main.zig
@@ -1,142 +1,118 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
+const AutoHashMap = std.AutoHashMap;
 
-const InputArrays = struct {
-    a: []u64,
-    b: []u64,
-};
+const Input = struct {
+    alloc: Allocator,
+    sorted_a: []const u64,
+    sorted_b: []const u64,
+    count_a: AutoHashMap(u64, u64),
+    count_b: AutoHashMap(u64, u64),
 
-const InputMaps = struct {
-    a: std.AutoHashMap(u64, u64),
-    b: std.AutoHashMap(u64, u64),
-};
+    pub fn init(path: []const u8, alloc: Allocator) !Input {
+        var file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
 
-fn readInputAsSortedArrays(path: []const u8, alloc: Allocator) !InputArrays {
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
+        var buf_reader = std.io.bufferedReader(file.reader());
+        var in_stream = buf_reader.reader();
 
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
+        var list_one = ArrayList(u64).init(alloc);
+        var list_two = ArrayList(u64).init(alloc);
+        defer list_one.deinit();
+        defer list_two.deinit();
 
-    var list_one = ArrayList(u64).init(alloc);
-    var list_two = ArrayList(u64).init(alloc);
-    defer list_one.deinit();
-    defer list_two.deinit();
+        var map_one = AutoHashMap(u64, u64).init(
+            alloc,
+        );
+        var map_two = AutoHashMap(u64, u64).init(
+            alloc,
+        );
+        errdefer {
+            map_one.deinit();
+            map_two.deinit();
+        }
 
-    var buf: [1024]u8 = undefined;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var it = std.mem.splitSequence(u8, line, "   ");
-        const one_s = it.first();
-        const two_s = it.next();
+        var buf: [1024]u8 = undefined;
+        while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            var it = std.mem.tokenize(u8, line, " ");
+            const one_s = it.next() orelse continue;
+            const two_s = it.next() orelse continue;
 
-        if (two_s) |val| {
             const one = try std.fmt.parseInt(u64, one_s, 10);
-            const two = try std.fmt.parseInt(u64, val, 10);
+            const two = try std.fmt.parseInt(u64, two_s, 10);
 
             try list_one.append(one);
             try list_two.append(two);
-        }
-    }
 
-    const one_arr = try list_one.toOwnedSlice();
-    const two_arr = try list_two.toOwnedSlice();
+            const one_entry = try map_one.getOrPutValue(one, 0);
+            one_entry.value_ptr.* += 1;
 
-    std.mem.sort(u64, one_arr, {}, std.sort.asc(u64));
-    std.mem.sort(u64, two_arr, {}, std.sort.asc(u64));
-
-    return .{
-        .a = one_arr,
-        .b = two_arr,
-    };
-}
-
-fn calcDistance(input: InputArrays) u64 {
-    var distance: u64 = 0;
-
-    for (0.., input.a) |i, a_val| {
-        const b_val = input.b[i];
-
-        if (b_val == a_val) {
-            continue;
+            const two_entry = try map_two.getOrPutValue(two, 0);
+            two_entry.value_ptr.* += 1;
         }
 
-        if (b_val > a_val) {
-            distance += b_val - a_val;
-        } else {
-            distance += a_val - b_val;
+        std.mem.sort(u64, list_one.items, {}, std.sort.asc(u64));
+        std.mem.sort(u64, list_two.items, {}, std.sort.asc(u64));
+
+        return .{
+            .alloc = alloc,
+            .sorted_a = try list_one.toOwnedSlice(),
+            .sorted_b = try list_two.toOwnedSlice(),
+            .count_a = map_one,
+            .count_b = map_two,
+        };
+    }
+
+    pub fn distance(self: Input) u64 {
+        var d: u64 = 0;
+
+        for (self.sorted_a, self.sorted_b) |a_val, b_val| {
+            if (b_val > a_val) {
+                d += b_val - a_val;
+            } else {
+                d += a_val - b_val;
+            }
         }
+
+        return d;
     }
 
-    return distance;
-}
+    pub fn similarity(self: Input) u64 {
+        var it = self.count_a.iterator();
+        var s: u64 = 0;
 
-fn day1part1(alloc: Allocator) !void {
-    const input = try readInputAsSortedArrays("day1.txt", alloc);
-    const distance = calcDistance(input);
+        while (it.next()) |item| {
+            const a_count = item.value_ptr.*;
+            const b_count = self.count_b.get(item.key_ptr.*) orelse 0;
 
-    std.debug.print("day 1, part 1 (distance): {d}\n", .{distance});
-}
-
-fn readInputAsCountMap(path: []const u8, alloc: Allocator) !InputMaps {
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
-
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-
-    var map_one = std.AutoHashMap(u64, u64).init(
-        alloc,
-    );
-    var map_two = std.AutoHashMap(u64, u64).init(
-        alloc,
-    );
-
-    var buf: [1024]u8 = undefined;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var it = std.mem.splitSequence(u8, line, "   ");
-        const one_s = it.first();
-        const two_s = it.next();
-
-        if (two_s) |val| {
-            const one = try std.fmt.parseInt(u64, one_s, 10);
-            const two = try std.fmt.parseInt(u64, val, 10);
-
-            const one_count = map_one.get(one) orelse 0;
-            const two_count = map_two.get(two) orelse 0;
-
-            try map_one.put(one, one_count + 1);
-            try map_two.put(two, two_count + 1);
+            s += a_count * b_count * item.key_ptr.*;
         }
+
+        return s;
     }
 
-    return InputMaps{
-        .a = map_one,
-        .b = map_two,
-    };
-}
-
-fn day1part2(alloc: Allocator) !void {
-    const input = try readInputAsCountMap("day1.txt", alloc);
-    var it = input.a.keyIterator();
-
-    var similarity: u64 = 0;
-
-    while (it.next()) |key| {
-        const key_deref = key.*;
-        const a_count = input.a.get(key_deref) orelse 0;
-        const b_count = input.b.get(key_deref) orelse 0;
-
-        similarity += a_count * b_count * key_deref;
+    pub fn deinit(self: *Input) void {
+        self.count_a.deinit();
+        self.count_b.deinit();
+        self.alloc.free(self.sorted_a);
+        self.alloc.free(self.sorted_b);
     }
-
-    std.debug.print("day 1, part 2 (similarity): {d}\n", .{similarity});
-}
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    try day1part1(allocator);
-    try day1part2(allocator);
+    var input = try Input.init("day1.txt", allocator);
+    defer input.deinit();
+
+    const d = input.distance();
+    std.debug.assert(d == 2066446);
+    std.debug.print("day 1, part 1 (distance): {d}\n", .{d});
+
+    const s = input.similarity();
+    std.debug.assert(s == 24931009);
+    std.debug.print("day 1, part 2 (similarity): {d}\n", .{s});
 }
