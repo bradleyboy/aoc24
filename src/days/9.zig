6b9fb46772d2122008601998d47f30734971927e
Brad Daily <brad@bradleyboy.com>
2024-12-12 21:02:12 -0500

    day 9 part 2

--- src/days/9.zig
+++ src/days/9.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const AuthHashMap = std.AutoHashMap;
 const BlockType = enum { file, free };
 
 pub fn run(alloc: Allocator) !void {
@@ -8,46 +9,57 @@ pub fn run(alloc: Allocator) !void {
     defer alloc.free(disk);
 
     // fixme: weird trailing whitespace in the input file
-    var pointer_r = disk.len - 2;
-    const pointer_r_remaining_s = disk[pointer_r];
-    var pointer_r_remaining = try std.fmt.parseInt(usize, &[1]u8{pointer_r_remaining_s}, 10);
+    const pointer_r_start = disk.len - 2;
 
     var output = ArrayList(usize).init(alloc);
     defer output.deinit();
 
-    for (0.., disk) |idx, block| {
-        if (idx >= pointer_r) {
-            while (pointer_r_remaining > 0) {
-                const id = pointer_r / 2;
-                pointer_r_remaining -= 1;
-                try output.append(id + 1);
-            }
+    var moves = AuthHashMap(usize, u1).init(alloc);
+    defer moves.deinit();
 
+    for (0..disk.len) |idx| {
+        if (idx >= pointer_r_start) {
             break;
         }
 
         const t = if (@mod(idx, 2) == 0) BlockType.file else BlockType.free;
 
-        const block_len = try std.fmt.parseInt(usize, &[1]u8{block}, 10);
+        const block = disk[idx];
+        var block_len = try std.fmt.parseInt(usize, &[1]u8{block}, 10);
         if (t == .file) {
             const id = idx / 2;
+            const check = moves.get(idx);
             for (0..block_len) |_| {
-                try output.append(id + 1);
+                if (check == null) {
+                    try output.append(id + 1);
+                } else {
+                    try output.append(0);
+                }
             }
         } else {
-            for (0..block_len) |_| {
-                if (pointer_r_remaining == 0) {
-                    // skip over free space to the next file block
-                    pointer_r -= 2;
-                    const next_pointer_r_remaining_s = disk[pointer_r];
-                    pointer_r_remaining = try std.fmt.parseInt(usize, &[1]u8{next_pointer_r_remaining_s}, 10);
-                }
+            var pointer_r = pointer_r_start;
+            while (block_len > 0 and pointer_r > idx) {
+                defer pointer_r -= 2;
 
-                if (pointer_r_remaining > 0) {
-                    const id = pointer_r / 2;
-                    pointer_r_remaining -= 1;
-                    try output.append(id + 1);
+                if (moves.get(pointer_r) != null) {
+                    continue;
                 }
+                const candidate_size_s = disk[pointer_r];
+                const candidate_size = try std.fmt.parseInt(usize, &[1]u8{candidate_size_s}, 10);
+
+                if (candidate_size <= block_len) {
+                    for (0..candidate_size) |_| {
+                        const id = pointer_r / 2;
+                        try output.append(id + 1);
+                        block_len -= 1;
+                    }
+
+                    try moves.put(pointer_r, 1);
+                }
+            }
+
+            for (0..block_len) |_| {
+                try output.append(0);
             }
         }
     }
@@ -59,8 +71,10 @@ pub fn run(alloc: Allocator) !void {
 
     // we store ids as 1-based so we can use zeros for free space
     for (0.., ret) |idx, d| {
-        const file_id = d - 1;
-        sum += file_id * idx;
+        if (d > 0) {
+            const file_id = d - 1;
+            sum += file_id * idx;
+        }
     }
 
     std.debug.print("day 9, sum: {d}\n", .{sum});
